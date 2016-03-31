d661ad78f206c4c1ae4de096d76ff596f7519958
Victor Velazquez <victor.velazquez@crowdint.com>
2016-03-31 17:13:41 -0600

    Add new book to the list

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,19 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.create(book_params)
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title)
+  end
 end
